7c086dfb961f9fca0a76a70fea0c19a51b23f48f
Hu Tianyi <oscarhuthu@gmail.com>
2018-06-20 02:23:33 +0800

    add fetch table

--- studentmanager.py
+++ studentmanager.py
@@ -15,17 +15,19 @@ class studentmanager(QtWidgets.QMainWindow,Ui_studentmanagerClass):
         self.statuslabel.setAlignment(QtCore.Qt.AlignCenter)
         self.savebutton.clicked.connect(self.saveresult)
         self.deletebutton.clicked.connect(self.deletetable)
-        self.deletebutton.clicked.connect(self.changetable)
 
         self.config = dict()
         self.config["mysqlserver"] = {
             "host": "localhost",
             "port": 3306,
             "password": "981012aA$",
-            "user": "root"
+            "user": "root",
+            "database": "sys"
         }
-        self.fetchtable()
+
         self.connectMysql()
+        self.showTableOption()
+        self.fetchtable()
 
     def saveresult(self):
         exit()
@@ -39,22 +41,51 @@ class studentmanager(QtWidgets.QMainWindow,Ui_studentmanagerClass):
     def statuschange(self, status):
         self.statuslabel.setText(status)
 
+    def showTableOption(self):
+        if self.statuslabel.text() != "已连接":
+            return
+        cursor = self.conn.cursor()
+        cursor.execute("SHOW TABLES")
+        result = cursor.fetchall()
+
+        for t in result:
+            self.tableoption.insertItem(result.index(t),t[0])
+
+        self.tablename = result[0][0]
+
     def connectMysql(self):
         config = self.config["mysqlserver"]
         try:
-            self.conn = mysql.connector.connect(host=config["host"],port=config["port"],user=config["user"],password=config["password"])
+            self.conn = mysql.connector.connect(host=config["host"],port=config["port"],user=config["user"],password=config["password"],database=config["database"])
         except:
             self.statuschange("连接异常")
         else:
             self.statuschange("已连接")
         
     def fetchtable(self):
+        cursor = self.conn.cursor()
+
+        cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='{}' AND TABLE_NAME='{}'".format(
+            self.config["mysqlserver"]["database"], self.tablename))
+        result = cursor.fetchall()
+        col = len(result)
+        col_label = [x[0] for x in result]
+
+        cursor.execute("SELECT COUNT(*) FROM {}.{}".format(self.config["mysqlserver"]["database"],self.tablename))
+        result = cursor.fetchall()
+        row = result[0][0]
+
+        cursor.execute("SELECT * FROM {}.{}".format(self.config["mysqlserver"]["database"],self.tablename))
+        content = cursor.fetchall()
+
         table = self.showtable
-        table.setRowCount(10)
-        table.setColumnCount(5)
-        table.setHorizontalHeaderLabels(["person","id","math","chinese","english"])
-        table.setItem(0,0,QtWidgets.QTableWidgetItem("xxx"))
-        table.show()
+        table.setRowCount(row)
+        table.setColumnCount(col)
+        table.setHorizontalHeaderLabels(col_label)
+
+        for i in range(row):
+            for j in range(col):
+                table.setItem(i,j,QtWidgets.QTableWidgetItem(str(content[i][j])))
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
@@ -62,4 +93,3 @@ if __name__ == '__main__':
 
     main_window.show()
     sys.exit(app.exec_())
-
