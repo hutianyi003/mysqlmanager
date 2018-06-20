6f152f51e419726f19b05ff89f626e022ad6b653
Hu Tianyi <oscarhuthu@gmail.com>
2018-06-20 10:51:25 +0800

    add delete & sort

--- studentmanager.py
+++ studentmanager.py
@@ -1,6 +1,8 @@
 import sys
 
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import QtWidgets,QtCore,QtGui
+
 from Ui_studentmanager import Ui_studentmanagerClass
 
 import mysql.connector
@@ -15,43 +17,88 @@ class studentmanager(QtWidgets.QMainWindow,Ui_studentmanagerClass):
         self.statuslabel.setAlignment(QtCore.Qt.AlignCenter)
         self.savebutton.clicked.connect(self.saveresult)
         self.deletebutton.clicked.connect(self.deletetable)
+        self.tableoption.currentIndexChanged.connect(self.tablechange)
+        self.showtable.horizontalHeader().sectionClicked.connect(self.headerclick)
+        self.showtable.itemClicked.connect(self.cursorClicked)
+        self.showtable.itemChanged.connect(self.tablechange)
 
         self.config = dict()
+        self.oldtable = dict()
         self.config["mysqlserver"] = {
             "host": "localhost",
             "port": 3306,
             "password": "981012aA$",
             "user": "root",
-            "database": "sys"
+            "database": "world"
         }
+        self.oldtable["row"] = 0
+        self.oldtable["change"] = []
+        self.tablename = ''
 
         self.connectMysql()
         self.showTableOption()
-        self.fetchtable()
 
     def saveresult(self):
-        exit()
+        reply = QMessageBox.information(self,"保存","是否确定将改动保存到数据库？",QMessageBox.Yes|QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            self.excuteSql("DROP TABLE {}.{}")
     
     def deletetable(self):
-        exit()
+        reply = QMessageBox.warning(self,"警告","该操作会永久删除当前表格，是否继续？",QMessageBox.Yes|QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            self.excuteSql("DROP TABLE {}.{}")
+        self.showTableOption()
 
     def changetable(self):
         exit()
     
+    def setserver(self):
+        config = self.config["mysqlserver"]
+        layout = QtWidgets.QFormLayout()
+        
+
+    def cursorClicked(self, index):
+        self.infolabel.setText("({2},{3}) ({0},{1})     ".format(
+            self.showtable.rowCount()-1, self.showtable.columnCount(), index.row(), index.column()))
+
+    def excuteSql(self, command):
+        if self.statuslabel.text() != "已连接":
+            return
+
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(command.format(self.config["mysqlserver"]["database"],self.tablename))
+            result = cursor.fetchall()
+        except:
+            return None
+        else:
+            return result
+
+    def headerclick(self, index):
+        self.showtable.sortItems(index,)
+        return
+    
     def statuschange(self, status):
         self.statuslabel.setText(status)
+
+    def tablechange(self):
+        self.tablename = self.tableoption.currentText()
+        self.fetchtable()
 
     def showTableOption(self):
         if self.statuslabel.text() != "已连接":
             return
-        cursor = self.conn.cursor()
-        cursor.execute("SHOW TABLES")
-        result = cursor.fetchall()
+        result = self.excuteSql("SHOW TABLES")
 
+        if result is None or len(result) == 0:
+            self.tableoption.clear()
+            self.showtable.clear()
+            return 
         for t in result:
             self.tableoption.insertItem(result.index(t),t[0])
 
         self.tablename = result[0][0]
+        self.fetchtable()
 
     def connectMysql(self):
         config = self.config["mysqlserver"]
@@ -63,6 +110,8 @@ class studentmanager(QtWidgets.QMainWindow,Ui_studentmanagerClass):
             self.statuschange("已连接")
         
     def fetchtable(self):
+        if self.tablename == None or self.tablename == '':
+            return
         cursor = self.conn.cursor()
 
         cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='{}' AND TABLE_NAME='{}'".format(
@@ -79,13 +128,27 @@ class studentmanager(QtWidgets.QMainWindow,Ui_studentmanagerClass):
         content = cursor.fetchall()
 
         table = self.showtable
-        table.setRowCount(row)
+        table.setRowCount(row+1)
         table.setColumnCount(col)
         table.setHorizontalHeaderLabels(col_label)
 
+        self.oldtable["row"] = row
         for i in range(row):
             for j in range(col):
-                table.setItem(i,j,QtWidgets.QTableWidgetItem(str(content[i][j])))
+                c = str(content[i][j])
+                item = QtWidgets.QTableWidgetItem()
+                try:
+                    float(c)
+                except:
+                    try:
+                        int(c)
+                    except:
+                        item.setData(QtCore.Qt.EditRole,c)
+                    else:
+                        item.setData(QtCore.Qt.EditRole,int(c))
+                else:
+                    item.setData(QtCore.Qt.EditRole,float(c))
+                table.setItem(i,j,item)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
